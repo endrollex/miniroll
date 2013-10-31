6f3d0ef786245e5ed537f4303ba59af59665fa74
endrollex <seebook@126.com>
2013-10-31 15:33:46 +0800

    html5 instead of xhtml1

--- index_top.php
+++ index_top.php
@@ -5,10 +5,9 @@
  * This file have some CSS class or JS, please modify them for wanted style.
 */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="zh-cn">
 <head>
-<meta content="zh-cn" http-equiv="Content-Language" />
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <script type="text/javascript" src="htmindex.js"></script>
 <?php
--- manage_dir/post_top.php
+++ manage_dir/post_top.php
@@ -7,10 +7,9 @@
 //if direct visit, exit
 if (!isset($manage_php)) exit();
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="zh-cn">
 <head>
-<meta content="zh-cn" http-equiv="Content-Language" />
 <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <script type="text/javascript" src="htmindex.js"></script>
 <link rel="SHORTCUT ICON" href="images/icon_4.ico"/>
