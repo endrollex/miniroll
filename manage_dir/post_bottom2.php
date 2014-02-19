a152dbcec4268bf0d64033299a2308ea117109c0
endrollex <seebook@126.com>
2014-02-19 11:29:35 +0800

    post_bottom.php add mobile style

--- manage_dir/post_bottom2.php
+++ manage_dir/post_bottom2.php
@@ -10,5 +10,14 @@
 if (!isset($manage_php)) exit();
 ?>
 </div><!--/trace.div_normal-->
+<script type="text/javascript">
+//<![CDATA[
+if (screen.availWidth) {
+	if (screen.availWidth < 760) {
+		document.getElementById("dom_index01").style.backgroundImage = "url('images/alge_bg1_320b.gif')";
+	}
+}
+//]]>
+</script>
 </body>
 </html>
