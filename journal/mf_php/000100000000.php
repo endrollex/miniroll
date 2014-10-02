45fe87ff5661be917f3d82c0e4808bfaba38a6d3
endrollex <seebook@126.com>
2014-10-02 19:58:49 +0800

    fix album

--- journal/mf_php/000100000000.php
+++ journal/mf_php/000100000000.php
@@ -19,26 +19,28 @@ $chunk_size = 20;
 //
 if (file_exists($album_post)) require($album_post);
 if (isset($albumi) && isset($albums) && isset($album_path)) {
+	$albumi = array_reverse($albumi);
+	$albums = array_reverse($albums);
 	$albumi_c = array_chunk($albumi, $chunk_size);
 	$albums_c = array_chunk($albums, $chunk_size);
-	$ix_chunk = count($albumi_c)-1;
+	$ix_chunk = 0;
 	if (isset($_GET['albp'])) if (isset($albumi_c[$_GET['albp']])) $ix_chunk = $_GET['albp'];
-	for ($ix_alb = count($albumi_c[$ix_chunk]); $ix_alb != 0; --$ix_alb) {
-		if (!isset($albumi_c[$ix_chunk][$ix_alb-1]) || !isset($albums_c[$ix_chunk][$ix_alb-1])) continue;
-		$album_imgurl = 's_'.$albumi_c[$ix_chunk][$ix_alb-1];
+	for ($ix_alb = 0; $ix_alb != count($albumi_c[$ix_chunk]); ++$ix_alb) {
+		if (!isset($albumi_c[$ix_chunk][$ix_alb]) || !isset($albums_c[$ix_chunk][$ix_alb])) continue;
+		$album_imgurl = 's_'.$albumi_c[$ix_chunk][$ix_alb];
 		echo '<div class="album_div1">';
-		echo '<a target="_blank" href="'.$album_path.$albumi_c[$ix_chunk][$ix_alb-1].'">';
+		echo '<a target="_blank" href="'.$album_path.$albumi_c[$ix_chunk][$ix_alb].'">';
 		echo '<img class="album_image" alt="'.$album_imgurl.'" src="'.$album_path.$album_imgurl.'" />';
 		echo '</a>';
 		echo '<div class="album_div2">';
 		//text link
-		echo $albums_c[$ix_chunk][$ix_alb-1];
+		echo $albums_c[$ix_chunk][$ix_alb];
 		echo '</div></div>';
 	}
 	echo '<div class="div_com_page">';
-	for ($ix_alb = count($albumi_c); $ix_alb != 0; --$ix_alb) {
-		if ($ix_chunk == ($ix_alb-1)) echo ' '.album_sel_ix($ix_alb-1).'';
-		else echo ' <a class="album" href="'.$album_page.($ix_alb-1).'">'.album_sel_ix($ix_alb-1).'</a>';
+	for ($ix_alb = 0; $ix_alb != count($albumi_c); ++$ix_alb) {
+		if ($ix_chunk == ($ix_alb)) echo ' '.album_sel_ix($ix_alb).'';
+		else echo ' <a class="album" href="'.$album_page.($ix_alb).'">'.album_sel_ix($ix_alb).'</a>';
 	}
 	echo '</div>';
 }
